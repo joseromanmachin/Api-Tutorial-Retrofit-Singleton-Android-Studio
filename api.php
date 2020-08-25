81c1665beaca42fdcfbe682c1fb4c1173abad89f
José Román Machín Hernández <machinhernandez.joseroman@gmaill.com>
2020-08-25 13:20:51 -0500

    commit inicial

---
+++ api.php
@@ -0,0 +1,166 @@
+<?php
+ $a = array (
+  0 => 
+  array (
+    'clave' => 'AGS',
+    'nombre' => 'AGUASCALIENTES',
+  ),
+  1 => 
+  array (
+    'clave' => 'BC',
+    'nombre' => 'BAJA CALIFORNIA',
+  ),
+  2 => 
+  array (
+    'clave' => 'BCS',
+    'nombre' => 'BAJA CALIFORNIA SUR',
+  ),
+  3 => 
+  array (
+    'clave' => 'CHI',
+    'nombre' => 'CHIHUAHUA',
+  ),
+  4 => 
+  array (
+    'clave' => 'CHS',
+    'nombre' => 'CHIAPAS',
+  ),
+  5 => 
+  array (
+    'clave' => 'CMP',
+    'nombre' => 'CAMPECHE',
+  ),
+  6 => 
+  array (
+    'clave' => 'CMX',
+    'nombre' => 'CIUDAD DE MEXICO',
+  ),
+  7 => 
+  array (
+    'clave' => 'COA',
+    'nombre' => 'COAHUILA',
+  ),
+  8 => 
+  array (
+    'clave' => 'COL',
+    'nombre' => 'COLIMA',
+  ),
+  9 => 
+  array (
+    'clave' => 'DGO',
+    'nombre' => 'DURANGO',
+  ),
+  10 => 
+  array (
+    'clave' => 'GRO',
+    'nombre' => 'GUERRERO',
+  ),
+  11 => 
+  array (
+    'clave' => 'GTO',
+    'nombre' => 'GUANAJUATO',
+  ),
+  12 => 
+  array (
+    'clave' => 'HGO',
+    'nombre' => 'HIDALGO',
+  ),
+  13 => 
+  array (
+    'clave' => 'JAL',
+    'nombre' => 'JALISCO',
+  ),
+  14 => 
+  array (
+    'clave' => 'MCH',
+    'nombre' => 'MICHOACAN',
+  ),
+  15 => 
+  array (
+    'clave' => 'MEX',
+    'nombre' => 'ESTADO DE MEXICO',
+  ),
+  16 => 
+  array (
+    'clave' => 'MOR',
+    'nombre' => 'MORELOS',
+  ),
+  17 => 
+  array (
+    'clave' => 'NAY',
+    'nombre' => 'NAYARIT',
+  ),
+  18 => 
+  array (
+    'clave' => 'NL',
+    'nombre' => 'NUEVO LEON',
+  ),
+  19 => 
+  array (
+    'clave' => 'OAX',
+    'nombre' => 'OAXACA',
+  ),
+  20 => 
+  array (
+    'clave' => 'PUE',
+    'nombre' => 'PUEBLA',
+  ),
+  21 => 
+  array (
+    'clave' => 'QR',
+    'nombre' => 'QUINTANA ROO',
+  ),
+  22 => 
+  array (
+    'clave' => 'QRO',
+    'nombre' => 'QUERETARO',
+  ),
+  23 => 
+  array (
+    'clave' => 'SIN',
+    'nombre' => 'SINALOA',
+  ),
+  24 => 
+  array (
+    'clave' => 'SLP',
+    'nombre' => 'SAN LUIS POTOSI',
+  ),
+  25 => 
+  array (
+    'clave' => 'SON',
+    'nombre' => 'SONORA',
+  ),
+  26 => 
+  array (
+    'clave' => 'TAB',
+    'nombre' => 'TABASCO',
+  ),
+  27 => 
+  array (
+    'clave' => 'TLX',
+    'nombre' => 'TLAXCALA',
+  ),
+  28 => 
+  array (
+    'clave' => 'TMS',
+    'nombre' => 'TAMAULIPAS',
+  ),
+  29 => 
+  array (
+    'clave' => 'VER',
+    'nombre' => 'VERACRUZ',
+  ),
+  30 => 
+  array (
+    'clave' => 'YUC',
+    'nombre' => 'YUCATAN',
+  ),
+  31 => 
+  array (
+    'clave' => 'ZAC',
+    'nombre' => 'ZACATECAS',
+  ),
+);
+
+print_r(json_encode($a));
+?>
